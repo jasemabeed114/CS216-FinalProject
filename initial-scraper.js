3cab7f1ecea8ac250c96b3bc29309aba37ae195c
Asim Hasan <reflective99@users.noreply.github.com>
2017-03-07 04:09:11 -0500

    Update initial-scraper.js

--- initial-scraper.js
+++ initial-scraper.js
@@ -3,71 +3,40 @@ var gplay = require('google-play-scraper');
 // Import JS File System variable
 var fs = require('fs');
 
-
-// Code to retrive top 5 Free Apps (5 just as an example, because I didn't tweak the query
-// with delays, but we can get data for thousands of apps)
-
-// A list of top 5 free apps
+// Code to retrive top 5 Free Apps (I'm using 5 files just as an example. This is 
+// because I didn't tweak the query with delays, which we'll have to do when we 
+// send Google requests for thousands of apps.
 
 // This promise returns a reply that the scraper receives from Google Play Store
+// and handles the the scraping flow
 var promise = gplay.list({
   collection: gplay.collection.TOP_FREE,
   num: 5
 })
-.then(data => writeListToFile(data))
-.then(data => fetchDetails(data))
-.then(data => fetchReviews(data));
+  .then(data => writeListToFile(data))
+  .then(data => fetchDetails(data))
+  .then(data => fetchReviews(data));
 
-
-// Name of the file where we will save the JSON (we dump all this data into SQL database later,
-// but for testing purposes I think a JSON file suffices)
+// This method writes the list of top 5 apps that we got as a reply, and saves it as a JSON dump.
+// (later we can dump all this data into SQL database, or just keep it in JSON format as 
+// it might be easier to play with, but for testing purposes I think a JSON suffices)
 function writeListToFile(data) {
-  var top5FreeAppsFile = "C:\\Users\\Asim Hasan\\workspace\\CS216\\project\\top5free.json";
-
+  var file = "C:\\Users\\Asim Hasan\\workspace\\CS216\\project\\top5free.json";
   // Write the list of top 20 Free Apps in a file
-  fs.writeFile(top5FreeAppsFile, JSON.stringify(data, null, 4), function(err) {
+  fs.writeFile(file, JSON.stringify(data, null, 4), function(err) {
     if(err) {
       console.log(err);
     } else {
-      console.log('Top 5 free apps data written to ' + top5FreeAppsFile + ' successfully');
+      console.log('Top 5 free apps data written to ' + file + ' successfully');
     }
   });
   return data;
 };
 
+// Code to retrive more detailed information about the above 5 apps that we just scraped 
 
-
-// Code to retrive more detailed information about the above 5 apps that we just scraped
-
-// For each app that we got from the above query, run this function which will write
-// its corresponding detailed info into a json file.
-
-function writeDataToFile(obj) {
-  console.log(obj)
-  //write the app's description to a file
-  var id = obj.appId;
-  var file = "C:\\Users\\Asim Hasan\\workspace\\CS216\\project\\json\\DETAILS-"+id+".json";
-  fs.writeFile(file, JSON.stringify(obj, null, 4), function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log('Description for app ' + id + ' saved in file ' + file);
-    }
-  });
-};
-
-function writeReviewsToFile(reviews, appId) {
-  console.log(reviews);
-  var file = "C:\\Users\\Asim Hasan\\workspace\\CS216\\project\\json\\REVIEWS-"+appId+".json";
-  fs.writeFile(file, JSON.stringify(reviews, null, 4), function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log('Reviews for app ' + appId + ' saved in file ' + file);
-    }
-  });
-  process.exit(0);
-};
+// For each app that we got from the above query, this function fetches more detailed information
+// about the app and writes it into a corresponding json file.
 
 function fetchDetails(appList) {
   appList.forEach(function (appObj) {
@@ -82,9 +51,9 @@ function fetchDetails(appList) {
   return appList;
 }
 
+// This function fetches the first two pages of reviews for each app that we scraped, 
+// and writes that data into a JSON file.
 
-
-// Code to get the first 2 pages of comments for each app and write it to a file
 function fetchReviews(appList) {
   appList.forEach(function (app) {
     var id = app.appId;
@@ -97,3 +66,34 @@ function fetchReviews(appList) {
     }).then(data => writeReviewsToFile(data, id));
   });
 }
+
+
+
+function writeDataToFile(obj) {
+  //console.log(obj)
+  //write the app's description to a file
+  var id = obj.appId;
+  var file = "C:\\Users\\Asim Hasan\\workspace\\CS216\\project\\json\\DETAILS-"+id+".json";
+  fs.writeFile(file, JSON.stringify(obj, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log('Description for app ' + id + ' saved in file ' + file);
+    }
+  });
+};
+
+// This function gets the first 2 pages of 
+
+function writeReviewsToFile(reviews, appId) {
+  console.log(reviews);
+  var file = "C:\\Users\\Asim Hasan\\workspace\\CS216\\project\\json\\REVIEWS-"+appId+".json";
+  fs.writeFile(file, JSON.stringify(reviews, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log('Reviews for app ' + appId + ' saved in file ' + file);
+    }
+  });
+  process.exit(0);
+};
